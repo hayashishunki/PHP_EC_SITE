ae41c82ab4c16ca7dde36cba24c599ce942d4315
hayashinoshun <s.y1014torimaru@gmail.com>
2022-07-20 22:00:46 +0900

    修正画面実装 pro_branch.phpより受け取った商品コードをもとに、 カテゴリー、商品名、価格、画像、商品の紹介文、の修正情報を入力する画面

--- product/pro_branch.php
+++ product/pro_branch.php
@@ -7,37 +7,37 @@
 #    print "<a href='staff_login.html'>ログイン画面へ</a>";
 #    exit();
 #} else {
-    if (isset($_POST["add"]) === true) {
-        header("Location:pro_add.php");
-        exit();
-    }
+if (isset($_POST["add"]) === true) {
+    header("Location:pro_add.php");
+    exit();
+}
 
-    if (isset($_POST["disp"]) === true) {
-        if (isset($_POST["code"]) === false) {
-            header("Location:pro_ng.php");
-            exit();
-        }
-        $code = $_POST["code"];
-        header("Location:pro_disp.php?code=" . $code);
+if (isset($_POST["disp"]) === true) {
+    if (isset($_POST["code"]) === false) {
+        header("Location:pro_ng.php");
         exit();
     }
+    $code = $_POST["code"];
+    header("Location:pro_disp.php?code=" . $code);
+    exit();
+}
 
-    if (isset($_POST["edit"]) === true) {
-        if (isset($_POST["code"]) === false) {
-            header("Location:pro_ng.php");
-            exit();
-        }
-        $code = $_POST["code"];
-        header("Location:pro_edit.php?code=" . $code);
+if (isset($_POST["edit"]) === true) {
+    if (isset($_POST["code"]) === false) {
+        header("Location:pro_ng.php");
         exit();
     }
+    $code = $_POST["code"];
+    header("Location:pro_edit.php?code=" . $code);
+    exit();
+}
 
-    if (isset($_POST["delete"]) === true) {
-        if (isset($_POST["code"]) === false) {
-            header("Location:pro_ng.php");
-            exit();
-        }
-        $code = $_POST["code"];
-        header("Location:pro_delete.php?code=" . $code);
+if (isset($_POST["delete"]) === true) {
+    if (isset($_POST["code"]) === false) {
+        header("Location:pro_ng.php");
         exit();
     }
+    $code = $_POST["code"];
+    header("Location:pro_delete.php?code=" . $code);
+    exit();
+}
